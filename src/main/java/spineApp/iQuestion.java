5b6d5022ca9fd67139382fe4cc50eefa3906a56f
WilmarvdPlas <s1137070@student.hsleiden.nl>
2022-10-27 14:35:18 +0200

    Sets iQuestion.java as the new main class and removes old main class "DemoApplication.java"

--- src/main/java/spineApp/iQuestion.java
+++ src/main/java/spineApp/iQuestion.java
@@ -1,4 +1,32 @@
 package spineApp;
 
-public class iQuestion {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Paths;
+@SpringBootApplication
+
+public class iQuestion extends Application {
+    public void start(Stage stage) {
+        try {
+            URL url = Paths.get("./src/main/resources/fxml/questionnaireEditor.fxml").toUri().toURL();
+            Parent root = FXMLLoader.load(url);
+            stage.setTitle("iQuestion");
+            stage.setScene(new javafx.scene.Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void main(String args[]) {
+        SpringApplication.run(iQuestion.class, args);
+
+        launch();
+    }
 }
