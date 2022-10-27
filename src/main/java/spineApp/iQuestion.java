32e4e16375fccb87761e8f2751804f50ae2b6700
Walid Bouhdif <wbouhdif@hotmail.com>
2022-10-27 15:20:37 +0200

    added login screen

--- src/main/java/spineApp/iQuestion.java
+++ src/main/java/spineApp/iQuestion.java
@@ -4,18 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Paths;
-@SpringBootApplication
+import java.util.Objects;
 
+@SpringBootApplication
 public class iQuestion extends Application {
     public void start(Stage stage) {
         try {
-            URL url = Paths.get("./src/main/resources/fxml/questionnaireEditor.fxml").toUri().toURL();
+            URL url = Paths.get("./src/main/resources/fxml/LoginScreen.fxml").toUri().toURL();
             Parent root = FXMLLoader.load(url);
             stage.setTitle("iQuestion");
             stage.setScene(new javafx.scene.Scene(root));
@@ -25,8 +25,6 @@ public class iQuestion extends Application {
         }
     }
     public static void main(String args[]) {
-        SpringApplication.run(iQuestion.class, args);
-
         launch();
     }
 }
