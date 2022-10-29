c317974e7707568f604f9353a8d8270f2c55ec4c
WilmarvdPlas <s1137070@student.hsleiden.nl>
2022-10-29 18:01:50 +0200

    Does a small clean-up of the AccountTypeController.

--- src/main/java/spineapp/backend/controllers/AccountTypeController.java
+++ src/main/java/spineapp/backend/controllers/AccountTypeController.java
@@ -3,9 +3,8 @@ package spineapp.backend.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import spineapp.backend.daos.AccountTypeDao;
-import spineapp.backend.models.AccountType;
 
 @RestController
 public class AccountTypeController {
-    @Autowired AccountTypeDao accountTypeDao;
+    @Autowired private AccountTypeDao accountTypeDao;
 }
