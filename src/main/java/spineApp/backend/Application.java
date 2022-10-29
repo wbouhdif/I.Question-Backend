58206b70822482f09fc2693b869ebfa76b51a5a7
WilmarvdPlas <s1137070@student.hsleiden.nl>
2022-10-29 12:36:40 +0200

    Rewrites Main class Application.java to remove references to javafx and work with spring boot.

--- src/main/java/spineApp/backend/Application.java
+++ src/main/java/spineApp/backend/Application.java
@@ -1,29 +1,12 @@
 package spineApp.backend;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Paths;
 
 @SpringBootApplication
-public class Application extends javafx.application.Application {
-    public void start(Stage stage) {
-        try {
-            URL url = Paths.get("./src/main/resources/fxml/LoginScreen.fxml").toUri().toURL();
-            Parent root = FXMLLoader.load(url);
-            stage.setTitle("iQuestion");
-            stage.setScene(new javafx.scene.Scene(root));
-            stage.setResizable(false);
-            stage.show();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+public class Application {
     public static void main(String args[]) {
-        launch();
+        SpringApplication.run(Application.class, args);
     }
 }
