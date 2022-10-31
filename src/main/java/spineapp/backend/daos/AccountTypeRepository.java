88e3e945ddfb572dc83f488aba5062c8bcbc4a42
WilmarvdPlas <s1137070@student.hsleiden.nl>
2022-10-31 12:41:33 +0100

    Adds findByName to AccountTypeRepository.

--- src/main/java/spineapp/backend/daos/AccountTypeRepository.java
+++ src/main/java/spineapp/backend/daos/AccountTypeRepository.java
@@ -1,12 +1,14 @@
 package spineapp.backend.daos;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import spineapp.backend.models.Account;
 import spineapp.backend.models.AccountType;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AccountTypeRepository extends JpaRepository<AccountType, UUID> {
+    Optional<Account> findByName(String name);
 }
