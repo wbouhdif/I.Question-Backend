f3e7001e80bcd5a22f8fb9be72acb92b1e8b5cfe
WilmarvdPlas <s1137070@student.hsleiden.nl>
2022-10-31 19:04:03 +0100

    Adds getAccountType, getAccountTypes and deleteAccountType to AccountTypeController.

--- src/main/java/spineapp/backend/controllers/AccountTypeController.java
+++ src/main/java/spineapp/backend/controllers/AccountTypeController.java
@@ -7,20 +7,37 @@ import org.springframework.web.bind.annotation.*;
 import spineapp.backend.models.AccountType;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping(path = "/api/account_type")
 public class AccountTypeController {
 
-    @Autowired private AccountTypeDAO accountTypeDAO;
+    private final AccountTypeDAO accountTypeDAO;
+
+    @Autowired
+    AccountTypeController(AccountTypeDAO accountTypeDAO) {
+        this.accountTypeDAO = accountTypeDAO;
+    }
+
+    @GetMapping
+    public List<AccountType> getAccountTypes() {
+        return accountTypeDAO.getAccountTypes();
+    }
+
+    @GetMapping (path = "{account_typeId}")
+    public Optional<AccountType> getAccountType(@PathVariable("account_typeId") UUID id) {
+        return accountTypeDAO.getAccountTypeById(id);
+    }
 
     @PostMapping
     public void createAccountType(@RequestBody AccountType accountType) {
         accountTypeDAO.createAccountType(accountType);
     }
 
-    @RequestMapping(value="/account_type", method=RequestMethod.GET)
-    public List<AccountType> readAccountTypes() {
-        return accountTypeDAO.getAccountTypes();
+    @DeleteMapping(path = "{account_typeId}")
+    public void deleteAccountType(@PathVariable("account_typeId") UUID id) {
+        accountTypeDAO.deleteAccountType(id);
     }
 }
