c905b90db66445183c7e16d324b7f7cf80896421
WilmarvdPlas <s1137070@student.hsleiden.nl>
2022-10-31 12:43:16 +0100

    Refactors AccountTypeDAO in AccountTypeController.

--- src/main/java/spineapp/backend/controllers/AccountTypeController.java
+++ src/main/java/spineapp/backend/controllers/AccountTypeController.java
@@ -2,9 +2,9 @@ package spineapp.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
-import spineapp.backend.daos.AccountTypeDao;
+import spineapp.backend.daos.AccountTypeDAO;
 
 @RestController
 public class AccountTypeController {
-    @Autowired private AccountTypeDao accountTypeDao;
+    @Autowired private AccountTypeDAO accountTypeDAO;
 }
