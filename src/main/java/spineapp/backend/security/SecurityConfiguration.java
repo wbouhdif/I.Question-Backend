2d9e9dcd08cbc3661ca1d8251c001a864aade3e9
WilmarvdPlas <s1137070@student.hsleiden.nl>
2022-11-03 11:06:53 +0100

    Adds SecurityConfiguration to project.

---
+++ src/main/java/spineapp/backend/security/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package spineapp.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.security.SecureRandom;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10, new SecureRandom());
+    }
+
+}
