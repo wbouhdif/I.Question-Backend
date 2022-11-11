c9110b9f9b9a4c973a066e657519528f78006f10
mikewww19 <unconfigured@null.spigotmc.org>
2022-11-11 16:49:55 +0100

    updated QuestionDAO and QuestionController for data access

--- src/main/java/spineapp/backend/controllers/QuestionController.java
+++ src/main/java/spineapp/backend/controllers/QuestionController.java
@@ -1,15 +1,29 @@
 package spineapp.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spineapp.backend.daos.QuestionDAO;
+import spineapp.backend.models.Question;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/question")
 public class QuestionController {
 
     private final QuestionDAO questionDAO;
+
+    public List<Question> getQuestions(){
+        return questionDAO.getQuestions();
+    }
+
+//    @PostMapping(path = "/create", consumes = MediaType.APPLICATION_JSON_VALUE)
+//    public void createNewQuestion(@RequestMapping Question question){
+//
+//    }
 
     @Autowired
     public QuestionController(QuestionDAO questionDAO) {
--- src/main/java/spineapp/backend/daos/QuestionDAO.java
+++ src/main/java/spineapp/backend/daos/QuestionDAO.java
@@ -2,6 +2,11 @@ package spineapp.backend.daos;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import spineapp.backend.models.Question;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Component
 public class QuestionDAO {
@@ -13,4 +18,23 @@ public class QuestionDAO {
         this.questionRepository = questionRepository;
     }
 
+    public void createQuestion(Question question){
+        questionRepository.save(question);
+    }
+
+    public List<Question> getQuestions(){
+        return questionRepository.findAll();
+    }
+
+    public Optional<Question> getQuestionById(UUID id){
+        return questionRepository.findById(id);
+    }
+
+    public void deleteQuestion(UUID id){
+        questionRepository.deleteById(id);
+    }
+
+    public boolean existsById(UUID id){
+        return questionRepository.existsById(id);
+    }
 }
