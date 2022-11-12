bb6f14ce429990c6cec728f4b8f3185d4d194771
WilmarvdPlas <s1137070@student.hsleiden.nl>
2022-11-12 11:25:10 +0100

    Adds getQuestionnaire() to QuestionnaireController and QuestionnaireDAO.

--- src/main/java/spineapp/backend/controllers/QuestionnaireController.java
+++ src/main/java/spineapp/backend/controllers/QuestionnaireController.java
@@ -1,9 +1,16 @@
 package spineapp.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spineapp.backend.daos.QuestionnaireDAO;
+import spineapp.backend.exceptions.EntityNotFoundException;
+import spineapp.backend.models.Questionnaire;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "api/questionnaire")
@@ -14,6 +21,15 @@ public class QuestionnaireController {
     @Autowired
     public QuestionnaireController(QuestionnaireDAO questionnaireDAO) {
         this.questionnaireDAO = questionnaireDAO;
+    }
+
+    @GetMapping(path = "{questionnaireId}")
+    public Optional<Questionnaire> getQuestionnaire(@PathVariable("questionnaireId") UUID id) throws EntityNotFoundException {
+        Optional<Questionnaire> questionnaire = questionnaireDAO.getQuestionnaireById(id);
+        if (questionnaire.isEmpty()) {
+            throw new EntityNotFoundException(id);
+        }
+        return questionnaire;
     }
 
 }
--- src/main/java/spineapp/backend/daos/QuestionnaireDAO.java
+++ src/main/java/spineapp/backend/daos/QuestionnaireDAO.java
@@ -2,6 +2,10 @@ package spineapp.backend.daos;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import spineapp.backend.models.Questionnaire;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Component
 public class QuestionnaireDAO {
@@ -12,5 +16,10 @@ public class QuestionnaireDAO {
     public QuestionnaireDAO(QuestionnaireRepository questionnaireRepository) {
         this.questionnaireRepository = questionnaireRepository;
     }
+
+    public Optional<Questionnaire> getQuestionnaireById(UUID id) {
+        return questionnaireRepository.findById(id);
+    }
+
 
 }
