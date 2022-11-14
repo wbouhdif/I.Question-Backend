24a40d716b1b097490414dcb171a744d3f26dbd5
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-11-14 22:40:27 +0100

    Adds createAnswer() to AnswerController.

--- src/main/java/spineapp/backend/controllers/AnswerController.java
+++ src/main/java/spineapp/backend/controllers/AnswerController.java
@@ -1,9 +1,12 @@
 package spineapp.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spineapp.backend.daos.AnswerDAO;
+import spineapp.backend.models.Answer;
 
 @RestController
 @RequestMapping(path = "api/answer")
@@ -15,5 +18,11 @@ public class AnswerController {
     public AnswerController(AnswerDAO answerDAO) {
         this.answerDAO = answerDAO;
     }
+
+    @PostMapping
+    public void createAnswer(@RequestBody Answer answer) {
+        answerDAO.createAnswer(answer);
+    }
+
 
 }
