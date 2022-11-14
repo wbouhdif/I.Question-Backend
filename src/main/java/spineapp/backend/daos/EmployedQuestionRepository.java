387e8e81ddd44424c0eb43217e1f78fcec1dfa5b
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-11-14 14:50:21 +0100

    Adds findAllByQuestionnaire to EmployedQuestionRepository.

--- src/main/java/spineapp/backend/daos/EmployedQuestionRepository.java
+++ src/main/java/spineapp/backend/daos/EmployedQuestionRepository.java
@@ -1,12 +1,15 @@
 package spineapp.backend.daos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import spineapp.backend.models.EmployedQuestion;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface EmployedQuestionRepository extends JpaRepository<EmployedQuestion, UUID> {
-
+    @Query("SELECT a FROM EmployedQuestion a WHERE a.questionnaire.id = ?1")
+    List<EmployedQuestion> findAllByQuestionnaire(UUID questionnaireId);
 }
