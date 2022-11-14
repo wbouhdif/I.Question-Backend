b20bbcca8ece05f4be30062ec7ccc8bc78ce4b0f
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-11-14 14:50:43 +0100

    Adds findAllByQuestion to OptionRepository.

--- src/main/java/spineapp/backend/daos/OptionRepository.java
+++ src/main/java/spineapp/backend/daos/OptionRepository.java
@@ -1,12 +1,16 @@
 package spineapp.backend.daos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import spineapp.backend.models.Option;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface OptionRepository extends JpaRepository<Option, UUID> {
+    @Query("SELECT a FROM Option a WHERE a.question.id = ?1")
+    List<Option> findAllByQuestion(UUID questionId);
 
 }
