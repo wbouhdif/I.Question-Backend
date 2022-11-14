59de9293810493cc401b436d9a3fdd50e4640b42
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-11-14 22:40:06 +0100

    Adds getAnsweredQuestionnaire() and createAnsweredQuestionnaire() to AnsweredQuestionnaireController.

--- src/main/java/spineapp/backend/controllers/AnsweredQuestionnaireController.java
+++ src/main/java/spineapp/backend/controllers/AnsweredQuestionnaireController.java
@@ -1,13 +1,14 @@
 package spineapp.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import spineapp.backend.daos.AnsweredQuestionnaireDAO;
+import spineapp.backend.exceptions.EntityNotFoundException;
 import spineapp.backend.models.AnsweredQuestionnaire;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "api/answered_questionnaire")
@@ -24,5 +25,20 @@ public class AnsweredQuestionnaireController {
     public List<AnsweredQuestionnaire> getAnsweredQuestionnaires() {
         return answeredQuestionnaireDAO.getAnsweredQuestionnaires();
     }
+
+    @GetMapping(path = "{answeredQuestionnaireId}")
+    public Optional<AnsweredQuestionnaire> getAnsweredQuestionnaire(@PathVariable("answeredQuestionnaireId") UUID id) throws EntityNotFoundException {
+        Optional<AnsweredQuestionnaire> answeredQuestionnaire = answeredQuestionnaireDAO.getAnsweredQuestionnaireById(id);
+        if (answeredQuestionnaire.isEmpty()) {
+            throw new EntityNotFoundException(id);
+        }
+        return answeredQuestionnaire;
+    }
+
+    @PostMapping
+    public UUID createAnsweredQuestionnaire(@RequestBody AnsweredQuestionnaire answeredQuestionnaire) {
+        return answeredQuestionnaireDAO.createAnsweredQuestionnaire(answeredQuestionnaire);
+    }
+
 
 }
