ce6e64c68264fdf954c12ae5dedd666a5d1d21cc
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-11-14 14:51:07 +0100

    Adds createOption and getOptionsByQuestion to OptionDAO.

--- src/main/java/spineapp/backend/daos/OptionDAO.java
+++ src/main/java/spineapp/backend/daos/OptionDAO.java
@@ -2,6 +2,10 @@ package spineapp.backend.daos;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import spineapp.backend.models.Option;
+
+import java.util.List;
+import java.util.UUID;
 
 @Component
 public class OptionDAO {
@@ -11,6 +15,14 @@ public class OptionDAO {
     @Autowired
     public OptionDAO(OptionRepository optionRepository) {
         this.optionRepository = optionRepository;
+    }
+
+    public void createOption(Option option){
+        optionRepository.save(option);
+    }
+
+    public List<Option> getOptionsByQuestion(UUID question) {
+        return optionRepository.findAllByQuestion(question);
     }
 
 }
