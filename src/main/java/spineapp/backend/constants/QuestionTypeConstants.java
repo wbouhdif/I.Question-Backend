38965203590f49ceb0c01e2fc6c42b51906c1494
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-11-14 10:33:49 +0100

    Adds QuestionTypeConstants to project.

---
+++ src/main/java/spineapp/backend/constants/QuestionTypeConstants.java
@@ -0,0 +1,6 @@
+package spineapp.backend.constants;
+
+public class QuestionTypeConstants {
+    public static final String MULTIPLE_CHOICE = "MULTIPLE_CHOICE";
+    public static final String OPEN = "OPEN";
+}
