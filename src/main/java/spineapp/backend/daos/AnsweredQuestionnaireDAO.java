d76e7aa830b674e8fa2813efabbaef94050f5365
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-11-14 22:38:50 +0100

    Adds getAnsweredQuestionnaireById() and createAnsweredQuestionnaire() to AnsweredQuestionnaireDAO.

--- src/main/java/spineapp/backend/daos/AnsweredQuestionnaireDAO.java
+++ src/main/java/spineapp/backend/daos/AnsweredQuestionnaireDAO.java
@@ -2,10 +2,12 @@ package spineapp.backend.daos;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import spineapp.backend.models.Account;
+import org.springframework.transaction.annotation.Transactional;
 import spineapp.backend.models.AnsweredQuestionnaire;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Component
 public class AnsweredQuestionnaireDAO {
@@ -19,6 +21,16 @@ public class AnsweredQuestionnaireDAO {
 
     public List<AnsweredQuestionnaire> getAnsweredQuestionnaires() {
         return answeredQuestionnaireRepository.findAll();
+    }
+
+    public Optional<AnsweredQuestionnaire> getAnsweredQuestionnaireById(UUID id) {
+        return answeredQuestionnaireRepository.findById(id);
+    }
+
+    @Transactional
+    public UUID createAnsweredQuestionnaire(AnsweredQuestionnaire answeredQuestionnaire) {
+        answeredQuestionnaireRepository.save(answeredQuestionnaire);
+        return answeredQuestionnaire.getId();
     }
 
 }
