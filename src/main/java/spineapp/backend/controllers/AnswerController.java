024e6cbbc3aca14870dddd8e6b7f5ccab0c9a1e6
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-11-15 11:12:28 +0100

    Adds getOptionsByQuestion() to AnswerController.

--- src/main/java/spineapp/backend/controllers/AnswerController.java
+++ src/main/java/spineapp/backend/controllers/AnswerController.java
@@ -1,27 +1,39 @@
 package spineapp.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import spineapp.backend.daos.AnswerDAO;
+import spineapp.backend.daos.AnsweredQuestionnaireDAO;
+import spineapp.backend.exceptions.EntityNotFoundException;
 import spineapp.backend.models.Answer;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "api/answer")
 public class AnswerController {
 
     private final AnswerDAO answerDAO;
+    private final AnsweredQuestionnaireDAO answeredQuestionnaireDAO;
 
     @Autowired
-    public AnswerController(AnswerDAO answerDAO) {
+    public AnswerController(AnswerDAO answerDAO, AnsweredQuestionnaireDAO answeredQuestionnaireDAO) {
         this.answerDAO = answerDAO;
+        this.answeredQuestionnaireDAO = answeredQuestionnaireDAO;
     }
 
     @PostMapping
     public void createAnswer(@RequestBody Answer answer) {
         answerDAO.createAnswer(answer);
+    }
+
+    @GetMapping(path = "answeredQuestionnaire={answeredQuestionnaire}")
+    public List<Answer> getOptionsByQuestion(@PathVariable("answeredQuestionnaire") UUID answeredQuestionnaire) throws EntityNotFoundException {
+        if (!answeredQuestionnaireDAO.existsById(answeredQuestionnaire)) {
+            throw new EntityNotFoundException(answeredQuestionnaire);
+        }
+        return answerDAO.getAnswersByAnsweredQuestionnaire(answeredQuestionnaire);
     }
 
 
