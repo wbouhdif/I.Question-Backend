72ce1ad1e2f91f61c5d7f22b05dc4bdb83f12f73
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-11-15 11:11:28 +0100

    Adds findAllByAnsweredQuestionnaire() to AnswerRepository.

--- src/main/java/spineapp/backend/daos/AnswerRepository.java
+++ src/main/java/spineapp/backend/daos/AnswerRepository.java
@@ -1,12 +1,15 @@
 package spineapp.backend.daos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import spineapp.backend.models.Answer;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, UUID> {
-
+    @Query("SELECT a FROM Answer a WHERE a.answeredQuestionnaire.id = ?1")
+    List<Answer> findAllByAnsweredQuestionnaire(UUID answeredQuestionnaireId);
 }
