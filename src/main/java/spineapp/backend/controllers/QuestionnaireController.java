5951ed85ba5f95a2ed3506f573e3e3cfd74b7e0e
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-11-29 14:33:40 +0100

    Adds javadoc comments to QuestionnaireController.

--- src/main/java/spineapp/backend/controllers/QuestionnaireController.java
+++ src/main/java/spineapp/backend/controllers/QuestionnaireController.java
@@ -1,7 +1,6 @@
 package spineapp.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import spineapp.backend.daos.QuestionnaireDAO;
 import spineapp.backend.exceptions.EntityNotFoundException;
@@ -15,19 +14,38 @@ import java.util.UUID;
 @RequestMapping(path = "api/questionnaire")
 public class QuestionnaireController {
 
-    private QuestionnaireDAO questionnaireDAO;
+    private final QuestionnaireDAO questionnaireDAO;
 
+    /**
+     * Constructs instance of QuestionnaireController with the questionnaireDAO via dependency injection.
+     * @param questionnaireDAO Parameter of type QuestionnaireDAO to be injected into.
+     */
     @Autowired
     public QuestionnaireController(QuestionnaireDAO questionnaireDAO) {
         this.questionnaireDAO = questionnaireDAO;
     }
 
+    /**
+     * Creates new entry in the questionnaire table in the database
+     * @param questionnaire Object of type Questionnaire to be posted.
+     * @return
+     * Returns generated UUID of posted questionnaire.
+     */
     @PostMapping
     public UUID createQuestionnaire(@RequestBody Questionnaire questionnaire) {
         return questionnaireDAO.createQuestionnaire(questionnaire);
     }
+
+    /**
+     * Finds questionnaire in database with given id.
+     * @param id ID of questionnaire to be looked for.
+     * @return
+     * Returns questionnaire with given id.
+     * @throws EntityNotFoundException
+     * Will throw exception if entity with given ID could not be found.
+     */
     @GetMapping(path = "{questionnaireId}")
-    public Optional<Questionnaire> getQuestionnaireID(@PathVariable("questionnaireId") UUID id) throws EntityNotFoundException {
+    public Optional<Questionnaire> getQuestionnaireById(@PathVariable("questionnaireId") UUID id) throws EntityNotFoundException {
         Optional<Questionnaire> questionnaire = questionnaireDAO.getQuestionnaireById(id);
         if (questionnaire.isEmpty()) {
             throw new EntityNotFoundException(id);
@@ -35,11 +53,22 @@ public class QuestionnaireController {
         return questionnaire;
     }
 
+    /**
+     * Find all questionnaires in database.
+     * @return
+     * Returns list of all questionnaires that were found in the database.
+     */
     @GetMapping
     public List<Questionnaire> getQuestionnaires() {
         return questionnaireDAO.getQuestionnaires();
     }
 
+    /**
+     * Attempts to delete questionnaire with given id from the database.
+     * @param id ID of questionnaire to be looked for.
+     * @throws EntityNotFoundException
+     * Will throw exception if entity with given ID could not be found.
+     */
     @DeleteMapping(path = "{questionnaireId}")
     public void deleteQuestionnaire(@PathVariable("questionnaireId") UUID id) throws EntityNotFoundException {
         if (!questionnaireDAO.existsById(id)) {
