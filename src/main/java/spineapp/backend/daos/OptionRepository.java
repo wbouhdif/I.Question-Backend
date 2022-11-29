7205408009a057de22330a62bc12967ebea96538
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-11-29 15:33:15 +0100

    Adds javadoc comments to and removes redundant methods from OptionRepository.

--- src/main/java/spineapp/backend/daos/OptionRepository.java
+++ src/main/java/spineapp/backend/daos/OptionRepository.java
@@ -4,18 +4,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import spineapp.backend.models.Option;
-import spineapp.backend.models.Question;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface OptionRepository extends JpaRepository<Option, UUID> {
+
+    /**
+     * Finds all options in database with given question.
+     * @param questionId ID of question to be looked for.
+     * @return
+     * Returns list of all options with given question that were found.
+     */
     @Query("SELECT a FROM Option a WHERE a.question.id = ?1")
     List<Option> findAllByQuestion(UUID questionId);
-
-    @Query("SELECT o FROM Option o WHERE o.text = ?1")
-    Optional<Option> findOptionByText(String text);
 
 }
