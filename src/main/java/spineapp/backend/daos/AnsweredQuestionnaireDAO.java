188673636671a0ee83ae814deb95f1af2db25343
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-11-29 15:31:06 +0100

    Adds javadoc comments to AnsweredQuestionnaireDAO.

--- src/main/java/spineapp/backend/daos/AnsweredQuestionnaireDAO.java
+++ src/main/java/spineapp/backend/daos/AnsweredQuestionnaireDAO.java
@@ -14,29 +14,62 @@ public class AnsweredQuestionnaireDAO {
 
     private final AnsweredQuestionnaireRepository answeredQuestionnaireRepository;
 
+    /**
+     * Constructs instance of AnsweredQuestionnaireDOA with the answeredQuestionnaireRepository via dependency injection.
+     * @param answeredQuestionnaireRepository Parameter of type AnsweredQuestionnaireRepository to be injected into.
+     */
     @Autowired
     public AnsweredQuestionnaireDAO(AnsweredQuestionnaireRepository answeredQuestionnaireRepository) {
         this.answeredQuestionnaireRepository = answeredQuestionnaireRepository;
     }
 
+    /**
+     * Finds all answered questionnaires in database.
+     * @return
+     * Returns list of all answered questionnaires found in database.
+     */
     public List<AnsweredQuestionnaire> getAnsweredQuestionnaires() {
         return answeredQuestionnaireRepository.findAll();
     }
 
+    /**
+     * Finds answered questionnaire in database with given id.
+     * @param id ID to be looked for.
+     * @return
+     * Returns answered questionnaire with given id.
+     */
     public Optional<AnsweredQuestionnaire> getAnsweredQuestionnaireById(UUID id) {
         return answeredQuestionnaireRepository.findById(id);
     }
 
+    /**
+     * Creates answered questionnaire in database.
+     * @param answeredQuestionnaire Answered questionnaire to be posted.
+     * @return
+     * Returns generated UUID of created answered questionnaire.
+     */
     @Transactional
     public UUID createAnsweredQuestionnaire(AnsweredQuestionnaire answeredQuestionnaire) {
         answeredQuestionnaireRepository.save(answeredQuestionnaire);
         return answeredQuestionnaire.getId();
     }
 
+    /**
+     * Checks if answered questionnaire with given ID exists in database.
+     * @param id ID of answered questionnaire to be looked for.
+     * @return
+     * Returns true or false depending on the result of existsById().
+     */
     public boolean existsById(UUID id){
         return answeredQuestionnaireRepository.existsById(id);
     }
 
+    /**
+     * Finds all answered questionnaires with given questionnaire.
+     * @param questionnaireId ID of questionnaire to be looked for.
+     * @return
+     * Returns list of all answered questionnaires with given questionnaire that were found.
+     */
     public List<AnsweredQuestionnaire> getAnsweredQuestionnairesByQuestionnaire(UUID questionnaireId) {
         return answeredQuestionnaireRepository.getAnsweredQuestionnairesByQuestionnaire(questionnaireId);
     }
