020360e702dc5bc85e4ed61d360ad6fae01b3959
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-11-29 14:33:22 +0100

    Adds javadoc comments to OptionController.

--- src/main/java/spineapp/backend/controllers/OptionController.java
+++ src/main/java/spineapp/backend/controllers/OptionController.java
@@ -17,23 +17,39 @@ public class OptionController {
     private final OptionDAO optionDAO;
     private final QuestionDAO questionDAO;
 
+    /**
+     * Constructs instance of OptionController with the optionDAO and questionDAO via dependency injection.
+     * @param optionDAO Parameter of type OptionDAO to be injected into.
+     * @param questionDAO Parameter of type QuestionDAO to be injected into.
+     */
     @Autowired
     public OptionController(OptionDAO optionDAO, QuestionDAO questionDAO) {
         this.optionDAO = optionDAO;
         this.questionDAO = questionDAO;
     }
-    
 
+    /**
+     * Creates new entry in option table in the database.
+     * @param option Object of type Option to be posted.
+     * @throws EntityNotFoundException
+     * Will throw exception if entity with given ID could not be found.
+     */
     @PostMapping
     public void createNewOption(@RequestBody Option option) throws EntityNotFoundException {
-
         if (optionDAO.getOptionByText(option.getText()).isPresent()){
             throw new EntityNotFoundException(option.getText());
         }
         optionDAO.createOption(option);
-
     }
 
+    /**
+     * Finds all options in database with given question.
+     * @param question ID of question to be looked for.
+     * @return
+     * Returns all options in database with given question.
+     * @throws EntityNotFoundException
+     * Will throw exception if entity with given ID could not be found.
+     */
     @GetMapping(path = "question={question}")
     public List<Option> getOptionsByQuestion(@PathVariable("question") UUID question) throws EntityNotFoundException {
         if (!questionDAO.existsById(question)) {
