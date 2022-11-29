5c8516e68f4ca111936f061951261613305fd492
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-11-29 15:30:43 +0100

    Adds javadoc comments to AnswerDAO.

--- src/main/java/spineapp/backend/daos/AnswerDAO.java
+++ src/main/java/spineapp/backend/daos/AnswerDAO.java
@@ -12,21 +12,41 @@ public class AnswerDAO {
 
     private final AnswerRepository answerRepository;
 
+    /**
+     * Constructs instance of AnswerDAO with the answerRepository via dependency injection.
+     * @param answerRepository Parameter of type AnswerRepository to be injected into.
+     */
     @Autowired
     public AnswerDAO(AnswerRepository answerRepository) {
         this.answerRepository = answerRepository;
     }
 
+    /**
+     * Creates entry in the answer table in the database
+     * @param answer Answer to be posted.
+     */
     public void createAnswer(Answer answer) {
         answerRepository.save(answer);
     }
 
+    /**
+     * Finds all answers in database with given answered questionnaire.
+     * @param answeredQuestionnaire Answered questionnaire to be looked for.
+     * @return
+     * Returns list of all answers with given answered questionnaire.
+     */
     public List<Answer> getAnswersByAnsweredQuestionnaire(UUID answeredQuestionnaire) {
         return answerRepository.findAllByAnsweredQuestionnaire(answeredQuestionnaire);
     }
+
+    /**
+     * Finds all answers in database with given employed question.
+     * @param employedQuestion Employed question to be looked for.
+     * @return
+     * Returns list of answers with given employed question.
+     */
     public List<Answer> getAnswersByEmployedQuestion(UUID employedQuestion) {
         return answerRepository.findAllByEmployedQuestion(employedQuestion);
     }
-
 
 }
