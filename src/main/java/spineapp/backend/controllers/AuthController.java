823a1c222883a47205da3ceeb6d15133c270ee5c
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-11-29 14:32:12 +0100

    Adds javadoc comments to AuthController.

--- src/main/java/spineapp/backend/controllers/AuthController.java
+++ src/main/java/spineapp/backend/controllers/AuthController.java
@@ -4,13 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import spineapp.backend.daos.AccountRepository;
-import spineapp.backend.models.Account;
 import spineapp.backend.models.LoginCredentials;
 import spineapp.backend.security.JWTUtil;
 
@@ -21,22 +18,26 @@ import java.util.Map;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    @Autowired private AccountRepository accountRepository;
-    @Autowired private JWTUtil jwtUtil;
-    @Autowired private AuthenticationManager authManager;
-    @Autowired private PasswordEncoder passwordEncoder;
+    private final JWTUtil jwtUtil;
+    private final AuthenticationManager authManager;
 
-    @PostMapping("/register")
-    public Map<String, Object> registerHandler(@RequestBody Account account){
-        if (accountRepository.findAccountByEmail(account.getEmail()).isPresent())
-            return Map.of("error", "Email already in use");
-        String encodedPass = passwordEncoder.encode(account.getPassword());
-        account.setPassword(encodedPass);
-        account = accountRepository.save(account);
-        String token = jwtUtil.generateToken(account.getEmail());
-        return Collections.singletonMap("jwt-token", token);
+    /**
+     * Constructs instance of AuthController with the jwtUtil and authManager via dependency injection.
+     * @param jwtUtil Parameter of type JWTUtil to be injected into.
+     * @param authManager Parameter of type AuthenticationManager to be injected into.
+     */
+    @Autowired
+    public AuthController(JWTUtil jwtUtil, AuthenticationManager authManager) {
+        this.jwtUtil = jwtUtil;
+        this.authManager = authManager;
     }
 
+    /**
+     * Handles all requests to the api/auth/login endpoint
+     * @param body LoginCredentials, sent by the user, to be checked.
+     * @return
+     * Returns a JWT token which will be used to authenticate requests of the logged-in user.
+     */
     @PostMapping("/login")
     public Map<String, Object> loginHandler(@RequestBody LoginCredentials body){
         try {
