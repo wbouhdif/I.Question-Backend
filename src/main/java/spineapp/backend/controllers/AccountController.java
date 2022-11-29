f2b4fb7cb6643e4c0d485353ea196a5378e62629
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-11-29 14:31:22 +0100

    Edits and adds to javadoc comments of AccountController.

--- src/main/java/spineapp/backend/controllers/AccountController.java
+++ src/main/java/spineapp/backend/controllers/AccountController.java
@@ -2,7 +2,6 @@ package spineapp.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import spineapp.backend.daos.AccountDAO;
@@ -25,10 +24,10 @@ public class AccountController {
     private final PasswordEncoder passwordEncoder;
 
     /**
-     * Constructs an account controller for the given DAO's and password encoder
-     * @param accountDAO DAO object of account
-     * @param accountTypeDAO DAO object of the account type
-     * @param passwordEncoder Encoder for the account's password
+     * Constructs instance of AccountController with the accountDAO, accountTypeDAO and passwordEncoder via dependency injection.
+     * @param accountDAO Parameter of type AccountDAO to be injected into.
+     * @param accountTypeDAO Parameter of type AccountTypeDAO to be injected into.
+     * @param passwordEncoder Parameter of type PasswordEncoder to be injected into.
      */
     @Autowired
     public AccountController(AccountDAO accountDAO, AccountTypeDAO accountTypeDAO, PasswordEncoder passwordEncoder) {
@@ -40,7 +39,7 @@ public class AccountController {
     /**
      * Gathers a list of all accounts found in the account DAO using the account DAO's getAccounts() function.
      * @return
-     * returns an arraylist of type Account.
+     * Returns an arraylist of type Account.
      */
     @GetMapping
     public List<Account> getAccounts() {
@@ -48,12 +47,12 @@ public class AccountController {
     }
 
     /**
-     * Finds a specific account linked to the given ID parameter
-     * @param id ID belonging to an account
+     * Finds a specific account linked to the given ID parameter.
+     * @param id ID belonging to an account.
      * @throws EntityNotFoundException
      * Will throw exception if entity with given id could not be found.
      * @return
-     * returns the account which corresponds with the given ID parameter
+     * Returns the account which corresponds with the given ID parameter.
      */
     @GetMapping (path = "{accountId}")
     public Optional<Account> getAccount(@PathVariable("accountId") UUID id) throws EntityNotFoundException {
@@ -64,7 +63,14 @@ public class AccountController {
         return account;
     }
 
-
+    /**
+     * Finds a specific account linked to the given email parameter.
+     * @param email Email belonging to an account.
+     * @return
+     * Returns the account which corresponds with the given email parameter.
+     * @throws EntityNotFoundException
+     * Will throw exception if entity with given id could not be found.
+     */
     @GetMapping (path = "email={email}")
     public Optional<Account> getAccountByEmail(@PathVariable("email") String email) throws EntityNotFoundException {
         Optional<Account> account = accountDAO.findAccountByEmail(email);
@@ -75,12 +81,12 @@ public class AccountController {
     }
 
     /**
-     * Registers a new account for a user
-     * @param account a new account instance to register the new account on
+     * Registers a new account for a user.
+     * @param account a new account instance to register the new account on.
      * @throws EmailTakenException
      * Will throw an exception in case the email address used to register the new user is already in the database.
      * @throws EntityNotFoundException
-     * Will throw an exception if the account on the given ID does not exist
+     * Will throw an exception if the account-type of the given ID does not exist.
      */
     @PostMapping(path = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void registerNewAccount(@RequestBody Account account) throws EmailTakenException, EntityNotFoundException {
@@ -99,6 +105,13 @@ public class AccountController {
         accountDAO.registerNewAccount(account);
     }
 
+    /**
+     * Sets the authorised attribute of account with given id to value of payload.
+     * @param id ID of account to be altered.
+     * @param authorised boolean which the authorised attribute of the account with given id will be set to.
+     * @throws EntityNotFoundException
+     * Will throw an exception if the account with given ID does not exist.
+     */
     @PutMapping(path = "{accountId}/authorised")
     public void setAuthorised(@PathVariable("accountId") UUID id, @RequestBody boolean authorised) throws EntityNotFoundException {
         if (!accountDAO.existsById(id)) {
@@ -109,10 +122,10 @@ public class AccountController {
     }
 
     /**
-     * Used to delete an existing account from the database
-     * @param id an ID belonging to an Account
+     * Used to delete an existing account from the database.
+     * @param id an ID belonging to an Account.
      * @throws EntityNotFoundException
-     * Will throw an exception if the account belonging to the given ID parameter does not exist
+     * Will throw an exception if the account belonging to the given ID parameter does not exist.
      */
     @DeleteMapping(path = "{accountId}")
     public void deleteAccount(@PathVariable("accountId") UUID id) throws EntityNotFoundException {
@@ -122,6 +135,11 @@ public class AccountController {
         accountDAO.deleteAccount(id);
     }
 
+    /**
+     * Sets password of account with given id to a randomly generated password.
+     * @param id ID of account to be altered.
+     * @param email Email where the new password is to be sent to.
+     */
     @PutMapping(path = "/newpassword/{accountId}")
     public void newPassword (@PathVariable("accountId") UUID id, @RequestBody String email) {
         String password = GeneratePassword.getInstance().generateNewPassword();
