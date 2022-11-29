b773430018d8d77d5fc06615c01130da3adf5bd6
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-11-29 15:30:07 +0100

    Edits and adds to javadoc comments of AccountTypeDAO.

--- src/main/java/spineapp/backend/daos/AccountTypeDAO.java
+++ src/main/java/spineapp/backend/daos/AccountTypeDAO.java
@@ -14,8 +14,8 @@ public class AccountTypeDAO {
     private final AccountTypeRepository accountTypeRepository;
 
     /**
-     * Constructs object of type AccountTypeDAO
-     * @param accountTypeRepository Instance of Account Type Repository
+     * Constructs instance of AccountDAO with the accountTypeRepository via dependency injection.
+     * @param accountTypeRepository Parameter of type AccountRepository to be injected into.
      */
     @Autowired
     public AccountTypeDAO(AccountTypeRepository accountTypeRepository) {
@@ -23,8 +23,8 @@ public class AccountTypeDAO {
     }
 
     /**
-     * Creates a new account Type within the Account type repository
-     * @param accountType Instance of Account Type
+     * Creates a new account Type within the Account type repository.
+     * @param accountType Instance of Account Type.
      */
     public void createAccountType(AccountType accountType) {
         accountTypeRepository.save(accountType);
@@ -33,40 +33,46 @@ public class AccountTypeDAO {
     /**
      * Finds all account types within the account type repository.
      * @return
-     * returns list of Type AccountType.
+     * Returns list of Type AccountType.
      */
     public List<AccountType> getAccountTypes() {
         return accountTypeRepository.findAll();
     }
 
     /**
-     * finds the account type for a given Account Type ID
-     * @param id ID belonging to an account type
+     * Finds the account type for a given Account Type ID.
+     * @param id ID belonging to an account type.
      * @return
-     * returns the account type belonging to the given ID
+     * Returns the account type belonging to the given ID.
      */
     public Optional<AccountType> getAccountTypeById(UUID id) {
         return accountTypeRepository.findById(id);
     }
 
     /**
-     * Deletes an Account Type from the account type repository
-     * @param id ID belonging to an account type
+     * Deletes an Account Type from the account type repository.
+     * @param id ID belonging to an account type.
      */
     public void deleteAccountType(UUID id) {
         accountTypeRepository.deleteById(id);
     }
 
     /**
-     * Finds whether an account exists within the repository
-     * @param id ID belonging to an account type
+     * Finds whether an account exists within the repository.
+     * @param id ID belonging to an account type.
      * @return
-     * returns the Account type for the given account type ID
+     * returns the Account type for the given account type ID.
      */
     public boolean existsById(UUID id) {
         return accountTypeRepository.existsById(id);
     }
 
+    /**
+     * Finds account type in repository with given name.
+     * @param name Name to be looked for.
+     * @return
+     * Returns account type with given name.
+     */
     public Optional<AccountType> getAccountTypeByName(String name) {
         return accountTypeRepository.findByName(name);
     }
