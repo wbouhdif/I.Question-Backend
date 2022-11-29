223f75a810a0d2a15c27517e38c45d1942c7e4a1
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-11-29 15:31:19 +0100

    Adds javadoc comments to AnsweredQuestionnaireRepository.

--- src/main/java/spineapp/backend/daos/AnsweredQuestionnaireRepository.java
+++ src/main/java/spineapp/backend/daos/AnsweredQuestionnaireRepository.java
@@ -3,7 +3,6 @@ package spineapp.backend.daos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import spineapp.backend.models.Answer;
 import spineapp.backend.models.AnsweredQuestionnaire;
 
 import java.util.List;
@@ -11,6 +10,13 @@ import java.util.UUID;
 
 @Repository
 public interface AnsweredQuestionnaireRepository extends JpaRepository<AnsweredQuestionnaire, UUID> {
+
+    /**
+     * Finds all answered questionnaires with given questionnaire.
+     * @param questionnaireId ID of questionnaire to be looked for.
+     * @return
+     * Returns list of all answered questionnaires with given questionnaire that were found.
+     */
     @Query("SELECT a FROM AnsweredQuestionnaire a WHERE a.questionnaire.id = ?1")
     List<AnsweredQuestionnaire> getAnsweredQuestionnairesByQuestionnaire(UUID questionnaireId);
 }
