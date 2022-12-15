7ea0914f7360f529a3a9a1c16b98854351296d6b
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2022-12-15 11:09:26 +0100

    Removes redundant fields from AccountType.

--- src/main/java/spineapp/backend/constants/AccountTypeConstants.java
+++ src/main/java/spineapp/backend/constants/AccountTypeConstants.java
@@ -5,8 +5,8 @@ import spineapp.backend.models.AccountType;
 
 public class AccountTypeConstants {
 
-    public static final AccountType ADMIN = new AccountType("Admin", true, true, true, true, false);
-    public static final AccountType CAREGIVER = new AccountType("Caregiver", false, true, false, false, true);
-    public static final AccountType SPINE = new AccountType("Spine", true, true, false, true, false);
+    public static final AccountType ADMIN = new AccountType("Admin");
+    public static final AccountType CAREGIVER = new AccountType("Caregiver");
+    public static final AccountType SPINE = new AccountType("Spine");
 
 }
--- src/main/java/spineapp/backend/models/AccountType.java
+++ src/main/java/spineapp/backend/models/AccountType.java
@@ -20,21 +20,6 @@ public class AccountType {
     @Column(nullable = false)
     private String name;
 
-    @Column(name = "can_manage_questionnaires", nullable = false)
-    private Boolean canManageQuestionnaires;
-
-    @Column(name = "can_fill_questionnaires", nullable = false)
-    private Boolean canFillQuestionnaires;
-
-    @Column(name = "can_authorise_accounts", nullable = false)
-    private Boolean canAuthoriseAccounts;
-
-    @Column(name = "can_view_anonymous_data", nullable = false)
-    private Boolean canViewAnonymousData;
-
-    @Column(name = "can_view_personalised_data", nullable = false)
-    private Boolean canViewPersonalisedData;
-
     /**
      * Constructs an empty account type without parameters. These can be defined after creation.
      */
@@ -43,24 +28,9 @@ public class AccountType {
     /**
      * Constructs a new Account Type
      * @param name Name
-     * @param canManageQuestionnaires A check to see whether the new account type can manage questionnaires
-     * @param canFillQuestionnaires A check to see whether the new account type can fill in existing questionnaires
-     * @param canAuthoriseAccounts A check to see whether the new account type can Authorise other accounts
-     * @param canViewAnonymousData A check to see whether the new account type can view anonymised data
-     * @param canViewPersonalisedData A check to see whether the new account type can view personalised data
      */
-    public AccountType(String name,
-                       Boolean canManageQuestionnaires,
-                       Boolean canFillQuestionnaires,
-                       Boolean canAuthoriseAccounts,
-                       Boolean canViewAnonymousData,
-                       Boolean canViewPersonalisedData) {
+    public AccountType(String name) {
         this.name = name;
-        this.canManageQuestionnaires = canManageQuestionnaires;
-        this.canFillQuestionnaires = canFillQuestionnaires;
-        this.canAuthoriseAccounts = canAuthoriseAccounts;
-        this.canViewAnonymousData = canViewAnonymousData;
-        this.canViewPersonalisedData = canViewPersonalisedData;
     }
 
 }
