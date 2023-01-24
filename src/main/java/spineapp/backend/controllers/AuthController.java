74ddff881bff28195e596d304d515751ac1c2446
Wilmar van der Plas <s1137070@student.hsleiden.nl>
2023-01-24 13:54:42 +0100

    Edits loginHandler() method to return both an account and a token only if the account is authorised.

--- src/main/java/spineapp/backend/controllers/AuthController.java
+++ src/main/java/spineapp/backend/controllers/AuthController.java
@@ -1,6 +1,8 @@
 package spineapp.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
@@ -8,11 +10,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import spineapp.backend.daos.AccountDAO;
+import spineapp.backend.models.Account;
 import spineapp.backend.models.LoginCredentials;
 import spineapp.backend.security.JWTUtil;
 
 import java.util.Collections;
-import java.util.Map;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -21,15 +25,18 @@ public class AuthController {
     private final JWTUtil jwtUtil;
     private final AuthenticationManager authManager;
 
+    private final AccountDAO accountDAO;
+
     /**
      * Constructs instance of AuthController with the jwtUtil and authManager via dependency injection.
      * @param jwtUtil Parameter of type JWTUtil to be injected into.
      * @param authManager Parameter of type AuthenticationManager to be injected into.
      */
     @Autowired
-    public AuthController(JWTUtil jwtUtil, AuthenticationManager authManager) {
+    public AuthController(JWTUtil jwtUtil, AuthenticationManager authManager, AccountDAO accountDAO) {
         this.jwtUtil = jwtUtil;
         this.authManager = authManager;
+        this.accountDAO = accountDAO;
     }
 
     /**
@@ -39,15 +46,27 @@ public class AuthController {
      * Returns a JWT token which will be used to authenticate requests of the logged-in user.
      */
     @PostMapping("/login")
-    public Map<String, Object> loginHandler(@RequestBody LoginCredentials body){
+    public ResponseEntity<Object> loginHandler(@RequestBody LoginCredentials body) {
         try {
-            UsernamePasswordAuthenticationToken authInputToken =
-                    new UsernamePasswordAuthenticationToken(body.getEmail(), body.getPassword());
+            UsernamePasswordAuthenticationToken authInputToken = new UsernamePasswordAuthenticationToken(body.getEmail(), body.getPassword());
             authManager.authenticate(authInputToken);
+
             String token = jwtUtil.generateToken(body.getEmail());
-            return Collections.singletonMap("jwt-token", token);
+            Account account = accountDAO.findAccountByEmail(body.getEmail()).get();
+
+            if (account.getAuthorised()) {
+
+                HashMap<Object, Object> responseBody = new HashMap<>();
+                responseBody.put("token", token);
+                responseBody.put("account", account);
+
+                return new ResponseEntity<>(responseBody, HttpStatus.ACCEPTED);
+
+            } else {
+                return new ResponseEntity<>(Collections.singletonMap("message", "Account not authorised"), HttpStatus.UNAUTHORIZED);
+            }
         }catch (AuthenticationException authExc){
-            throw new RuntimeException("Invalid Login Credentials");
+            return new ResponseEntity<>(Collections.singletonMap("message", "Invalid Login Credentials"), HttpStatus.NOT_ACCEPTABLE);
         }
     }
 }
